dc5e496d33aa934a27e4005ba7a9a6216f13b074
Suniljadhav80 <121239922+Suniljadhav80@users.noreply.github.com>
2022-12-22 19:38:56 +0530

    Add files via upload

---
+++ fourth.js
@@ -0,0 +1,27 @@
+const boxes = document.querySelectorAll('.box');
+const text = document.querySelector('#heading');
+const strategy = document.querySelector('#strategy');
+const restartBtn = document.querySelector('#restart');
+const drawBoard = () => {
+    boxes.forEach((box, i) => {
+      let styleString = '';
+      if (i < 3) {
+        styleString += 'border-bottom: 3px solid var(--text);';
+      }
+      if (i % 3 === 0) {
+        styleString += 'border-right: 3px solid var(--text);';
+      }
+      if (i % 3 === 2) {
+        styleString += 'border-left: 3px solid var(--text);';
+      }
+      if (i > 5) {
+        styleString += 'border-top: 3px solid var(--text);';
+      }
+      box.style = styleString;
+      box.addEventListener('click', boxClicked);
+    });
+  };
+  const spaces = [];
+const tick_circle = 'O';
+const tick_x = 'X';
+let currentPlayer = tick_circle;
